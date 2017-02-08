16610f59cc2e4052909b4c0d7845fca3190416e2
Yan Chi Ku <cyck.ku@gmail.com>
2017-02-08 21:50:35 +1100

    finish basic logic with no rate update, no timestamp

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,45 +14,90 @@
 //     console.log("exists");
 // }
 
+var rate = 23.50; // default val
+
 // 1. If online, fetch new rate & update timestamp
 if (navigator.onLine) {
-    // $.getJSON("http://api.fixer.io/latest", demo)
-    console.log("FETCHING")
-    $('#status').html("<span class='green'>●</span>");
-} else {
-    console.log("OFFLINE")
-    $('#status').html("<span class='red'>●</span>");
-}
+    var success = false;
+    // 1b. update rate
+    // var demo = function(data) {
+    //   fx.rates = data.rates;
+    //   var rate = fx(1).from("AUD").to("HKD");
+    //   console.log("api: 1 = $" + rate.toFixed(2));
+    //   rate = rate.toFixed(2);
+    //   success = true;
+    // }
+    // $.getJSON("http://api.fixer.io/latest", demo);
+    if (success) {
+        $('#status').html("<span class='green'>●</span>");
+    } else {
+        // online but can't fetch
+        $('#status').html("<span class='red'>●</span>");
+    }
+} // else grey dot
 
 // 2. display rate
-// fx.rates = data.rates
-// var rate = fx(1).from("GBP").to("USD")
-// console.log("api: 1 = $" + rate.toFixed(4))
-
+$('#ratio').html(rate);
 
 // 3. display new timestamp & store
 // setTimeout(function() {
 //     $('#last-updated').html(lastUpdated.toUTCString());
-
 // }, 500);
 
-function fetchOldTimestamp() {
-    var fileName = 'assets/txt/lastUpdated.txt';
+// 4. ux
+$('#aud').on('click', function() {
+    $('#twd').val("");
+    $('#aud').val("");
+});
 
-    try {
-        $.get(fileName, function(data) {
-            console.log(fileName);
-            console.log(data);
-            if (data.match(/^20[0-9]{2}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z/)) {
-                // valid date string
-                console.log("VALID");
-                lastUpdated = new Date(data);
-                console.log('last updated: ' + lastUpdated);
-            } else {
-                console.log('INVALID');
-            }
-        }, 'text');
-    } catch (e) {
-        console.warn(e);
+$('#twd').on('click', function() {
+    $('#twd').val("");
+    $('#aud').val("");
+});
+
+$('#aud').on('keyup', function() {
+    var toConvert = $('#aud').val();
+    console.log(toConvert);
+    var toDisplay = convert(toConvert, true);
+    $('#twd').val(toDisplay);
+});
+
+$('#twd').on('keyup', function() {
+    var toConvert = $('#twd').val();
+    console.log(toConvert);
+    var toDisplay = convert(toConvert, false);
+    $('#aud').val(toDisplay);
+});
+
+function convert(toConvert, isAud) {
+    var returnVal;
+    if (isAud) {
+        returnVal = toConvert * rate;
+        console.log(toConvert + " * " + rate + " = " + returnVal);
+    } else {
+        returnVal = toConvert / rate;
+        console.log(toConvert + " / " + rate + " = " + returnVal);
     }
+    return returnVal;
 }
+
+// function fetchOldTimestamp() {
+//     var fileName = 'assets/txt/lastUpdated.txt';
+
+//     try {
+//         $.get(fileName, function(data) {
+//             console.log(fileName);
+//             console.log(data);
+//             if (data.match(/^20[0-9]{2}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z/)) {
+//                 // valid date string
+//                 console.log("VALID");
+//                 lastUpdated = new Date(data);
+//                 console.log('last updated: ' + lastUpdated);
+//             } else {
+//                 console.log('INVALID');
+//             }
+//         }, 'text');
+//     } catch (e) {
+//         console.warn(e);
+//     }
+// }
