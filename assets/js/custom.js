d3450d479ea548c226282d4e79522abf482c66b1
duubyPlz <duubyplz@hotmail.com>
2017-12-25 22:01:36 +1100

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,50 +1,63 @@
-// jQuery(document).ready(function() {
-//   jQuery("time.timeago").timeago();
-// });
+// init
+jQuery(document).ready(function() {
+  jQuery("time.timeago").timeago();
+});
 
-// // 0. initialise previous timestamp
-// var lastUpdated = Cookies.get('lastUpdated');
-// if (lastUpdated == null) {
-//     var newTimestamp = new Date("2017-02-08T12:00:00Z");
-//     Cookies.set('lastUpdated', newTimestamp);
-//     Cookies.set('hi', 'hello');
-//     console.log(Cookies.get());
-//     console.log(Cookies.get('hi'));
-// } else {
-//     console.log("exists");
-// }
+// toggle all tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+// 0. initialise previous timestamp
+var lastUpdated = Cookies.get('lastUpdated');
+if (lastUpdated == null) {
+    console.log('ASDF');
+    lastUpdated = new Date("2017-02-08T12:00:00Z");
+    Cookies.set('lastUpdated', lastUpdated);
+    Cookies.set('name', 'value');
+} else {
+    console.log("exists");
+}
 
 var rate = 23.50; // default val
+var success = false;
+
+    var herp = Cookies.get('name');
+    console.log(Cookies.get());
+    console.log(Cookies.get('lastUpdated'));
+    console.log(herp);
 
 // 1. If online, fetch new rate & update timestamp
 if (navigator.onLine) {
-    var success = false;
     // 1b. update rate
-    // var demo = function(data) {
-    //   fx.rates = data.rates;
-    //   var rate = fx(1).from("AUD").to("HKD");
-    //   console.log("api: 1 = $" + rate.toFixed(2));
-    //   rate = rate.toFixed(2);
-    //   success = true;
-    // }
-    // $.getJSON("http://api.fixer.io/latest", demo);
-    if (success) {
-        $('#status').html("<span class='green'>●</span>");
-    } else {
-        // online but can't fetch
-        $('#status').html("<span class='red'>●</span>");
+    var demo = function(data) {
+      fx.rates = data.rates;
+      rate = fx(1).from("AUD").to("HKD");
+      console.log("api: 1 = $" + rate.toFixed(3));
+
+      // 2. Display rate
+      $('#ratio').html(rate.toFixed(3));
+
+      success = true;
     }
+    $.getJSON("http://api.fixer.io/latest", demo);
+
+    setTimeout(function() {
+        console.log(success);
+        if (success) {
+            $('#status').html("<span class='green'>&#x25cf;</span>");
+        } else {
+            // online but can't fetch
+            $('#status').html("<span class='red'>&#x25cf;</span>");
+        }
+
+        // 3. display new timestamp & store
+        $('#last-updated').html(lastUpdated.toUTCString());
+    }, 500);
 } // else grey dot
 
-// 2. display rate
-$('#ratio').html(rate);
-
-// 3. display new timestamp & store
-// setTimeout(function() {
-//     $('#last-updated').html(lastUpdated.toUTCString());
-// }, 500);
-
 // 4. ux
+// > a) input fields
 $('#aud').on('click', function() {
     $('#twd').val("");
     $('#aud').val("");
@@ -69,6 +82,9 @@ $('#twd').on('keyup', function() {
     $('#aud').val(toDisplay);
 });
 
+// > b) currency selector
+// $('#currency.active')
+
 function convert(toConvert, isAud) {
     var returnVal;
     if (isAud) {
@@ -81,23 +97,23 @@ function convert(toConvert, isAud) {
     return returnVal;
 }
 
-// function fetchOldTimestamp() {
-//     var fileName = 'assets/txt/lastUpdated.txt';
+function fetchOldTimestamp() {
+    var fileName = 'assets/txt/lastUpdated.txt';
 
-//     try {
-//         $.get(fileName, function(data) {
-//             console.log(fileName);
-//             console.log(data);
-//             if (data.match(/^20[0-9]{2}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z/)) {
-//                 // valid date string
-//                 console.log("VALID");
-//                 lastUpdated = new Date(data);
-//                 console.log('last updated: ' + lastUpdated);
-//             } else {
-//                 console.log('INVALID');
-//             }
-//         }, 'text');
-//     } catch (e) {
-//         console.warn(e);
-//     }
-// }
+    try {
+        $.get(fileName, function(data) {
+            console.log(fileName);
+            console.log(data);
+            if (data.match(/^20[0-9]{2}\-[0-9]{2}\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z/)) {
+                // valid date string
+                console.log("VALID");
+                lastUpdated = new Date(data);
+                console.log('last updated: ' + lastUpdated);
+            } else {
+                console.log('INVALID');
+            }
+        }, 'text');
+    } catch (e) {
+        console.warn(e);
+    }
+}
