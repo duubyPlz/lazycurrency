9e105e57922b3407a97ad3f59e1b95a8a3f98723
duubyPlz <duubyplz@hotmail.com>
2024-03-22 02:04:46 +1100

    display rate

--- src/ui/footer/rate/index.tsx
+++ src/ui/footer/rate/index.tsx
@@ -1,10 +1,23 @@
 import Status from './status';
 import Number from './number';
+import {
+  useActiveCurrency,
+  useRates,
+} from '../../../state/calculator/selectors';
 
-const Rate = () => (
-  <div>
-    rate <Number>1</Number>:<Number>--</Number> <Status />
-  </div>
-);
+// TODO Logic: Pull rate from sweet state
+const Rate = () => {
+  const rates = useRates()[0];
+  const activeCurrency = useActiveCurrency()[0];
+
+  const currentRate = rates[activeCurrency]?.toString() ?? '--';
+
+  return (
+    <div>
+      rate <Number>1</Number>:<Number>{currentRate}</Number>{' '}
+      <Status />
+    </div>
+  );
+};
 
 export default Rate;
