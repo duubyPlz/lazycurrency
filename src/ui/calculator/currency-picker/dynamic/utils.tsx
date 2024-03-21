4ac34a0d6667febffcc041000989165cd6598b97
duubyPlz <duubyplz@hotmail.com>
2024-03-22 01:53:43 +1100

    cleanup

--- src/ui/calculator/currency-picker/dynamic/utils.tsx
+++ src/ui/calculator/currency-picker/dynamic/utils.tsx
@@ -7,22 +7,19 @@ export const getBottomCurrencies = (): CurrencyType[] =>
     (currencyType) => currencyType !== CurrencyType.AUD,
   );
 
+// TODO Tests
 export const calculateShiftWidth = (
   ref: RefObject<HTMLSpanElement>,
 ): number => {
   const currentElement = ref?.current;
-  console.log('CURRENT EMEN', currentElement);
+
   const parentWidth = currentElement?.offsetParent?.clientWidth;
-  console.log('PARENTS WIDTH!!!!!', parentWidth);
   const offsetLeft = currentElement?.offsetLeft;
-  console.log('OFFSET LEFT', offsetLeft);
   const offsetWidth = currentElement?.offsetWidth;
-  console.log('OFFSET WIDTH', offsetWidth);
 
   const toTransform =
     parentWidth && offsetLeft && offsetWidth
       ? parentWidth - (offsetLeft + offsetWidth)
       : 0;
-  console.log('TO TRANSFORM', toTransform);
   return toTransform - toTransform / PADDING;
 };
